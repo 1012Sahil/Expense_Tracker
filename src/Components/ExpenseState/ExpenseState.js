16167f20788c3b4d4407526fa148c2bffa4c93ad
SAHIL AGARWAL <sahil.agarwal1012@gmail.com>
2023-07-01 20:49:13 +0530

    Added year selector

--- src/Components/ExpenseState/ExpenseState.js
+++ src/Components/ExpenseState/ExpenseState.js
@@ -4,9 +4,21 @@ import styles from "./ExpenseState.module.css";
 const ExpenseState = (props) => {
   return (
     <section className={styles.state}>
-      <div className={styles.balance}>
-        <h3>YOUR BALANCE</h3>
-        <p>$5000</p>
+      <div className={styles['year-control']}>
+        <div className={styles.balance}>
+          <h3>YOUR BALANCE</h3>
+          <p>$5000</p>
+        </div>
+        <div className={styles.yearSelector} >
+          <label htmlFor="year">Select Year</label>
+          <select id="year">
+            <option hidden disabled selected value></option>
+            <option value="2003">2003</option>
+            <option value="2004">2004</option>
+            <option value="2005">2005</option>
+            <option value="2006">2006</option>
+          </select>
+        </div>
       </div>
 
       <div className={styles.curState}>
